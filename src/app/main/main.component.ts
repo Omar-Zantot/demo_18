bdb67c05df874d01bc051bfb758069b1102f14af
Omar-Zantout <omar82199@gmail.com>
2024-08-18 22:23:42 +0300

    Task 1

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+}
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+})
+export class MainComponent {
+  userName!: string;
+
+  resetUserName(): void {
+    this.userName = '';
+  }
+}
